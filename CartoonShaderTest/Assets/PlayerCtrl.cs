0663d2d1ad1a508db6f228aae9befff79a6b2385
jk-kim113 <57799166+jk-kim113@users.noreply.github.com>
2021-06-09 23:50:51 +0900

    카툰 셰이더

---
+++ CartoonShaderTest/Assets/PlayerCtrl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    [SerializeField]
+    float _speed, _tilt;
+
+    Rigidbody _myRB;
+
+    private void Start()
+    {
+        _myRB = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(horizontal, vertical, 0);
+
+
+        _myRB.velocity = dir.normalized * _speed;
+
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, mXMin, mXMax),
+        //                                 transform.position.y,
+        //                                 Mathf.Clamp(transform.position.z, mZMin, mZMax));
+
+        _myRB.rotation = Quaternion.Euler(90, 0, 180 +_tilt * horizontal);
+    }
+}
