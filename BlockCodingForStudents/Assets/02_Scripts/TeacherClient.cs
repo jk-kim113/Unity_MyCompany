68e5720a0447c9fddb1965227496d49d4990ec33
JongKyeong_Kim <58102022+HNCSoo@users.noreply.github.com>
2021-06-11 17:59:46 +0900

    210611

---
+++ BlockCodingForStudents/Assets/02_Scripts/TeacherClient.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+
+public class TeacherClient : MonoBehaviour
+{
+    static TeacherClient _uniqueInstance;
+    public static TeacherClient _instance { get { return _uniqueInstance; } }
+
+    const string _ip = "127.0.0.1";
+    const int _port = 80;
+
+    Socket _server;
+
+    bool _isConnect = false;
+
+    private void Awake()
+    {
+        _uniqueInstance = this;
+    }
+
+    private void Start()
+    {
+        //ConnectServer();
+    }
+
+    public void ConnectServer()
+    {
+        _isConnect = Connect(_ip, _port);
+    }
+
+    bool Connect(string ipAddress, int port)
+    {
+        try
+        {
+            if (!_isConnect)
+            {
+                // make socket
+                _server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                _server.Connect(ipAddress, port);
+
+                return true;
+            }
+        }
+        catch (System.Exception ex)
+        {
+            // 메세지 창에 띄운다.
+            Debug.Log(ex.Message);
+        }
+
+        return false;
+    }
+}
---
+++ BlockCodingForStudents/Assets/02_Scripts/TeacherMainUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeacherMainUI : MonoBehaviour
+{
+    public enum eSchoolName
+    {
+        중동고등학교,
+        휘문고등학교,
+        경기고등학교,
+        숙명여자고등학교,
+
+        max
+    }
+
+    [SerializeField]
+    Dropdown _schoolListDropdown;
+    [SerializeField]
+    InputField _gradeInputField;
+    [SerializeField]
+    InputField _groupInputField;
+
+    private void Start()
+    {
+        List<string> temp = new List<string>();
+
+        for (int n = 0; n < (int)eSchoolName.max; n++)
+            temp.Add(((eSchoolName)n).ToString());
+
+        _schoolListDropdown.ClearOptions();
+        _schoolListDropdown.AddOptions(temp);
+    }
+
+    public void EnterBtn()
+    {
+        if(!string.IsNullOrEmpty(_gradeInputField.text) && !string.IsNullOrEmpty(_groupInputField.text))
+        {
+
+        }
+    }
+}
